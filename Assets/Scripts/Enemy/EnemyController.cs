633083c18162f88c77ec7e194f481c04e4965970
TrungKenbi <trungkenbi@hotmail.com>
2021-09-14 12:51:16 +0700

    Add simple enemy

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float minimum = -3.0f;
+    public float maximum = 3.0f;
+
+    private static float t = 0.0f;
+
+    private void FixedUpdate()
+    {
+        Vector2 newPos = transform.localPosition;
+        newPos.x = Mathf.Lerp(minimum, maximum, t);
+
+        transform.localPosition = newPos;
+
+        t += 0.5f * Time.fixedDeltaTime;
+
+        if (t > 3.0f)
+        {
+            float temp = maximum;
+            maximum = minimum;
+            minimum = temp;
+            t = 0.0f;
+        }
+    }
+}
